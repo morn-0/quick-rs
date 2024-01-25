6029a591c8abf938cec709bceb84e7348ee1b5c7
morn-0 <90960002+morn-0@users.noreply.github.com>
2024-01-25 17:38:02 +0800

    Fix unreleased call parameters

--- src/function.rs
+++ src/function.rs
@@ -14,21 +14,31 @@ impl Function {
         Ok(Function { value })
     }
 
-    pub fn call(&self, args: Vec<JSValueRef>) -> Result<JSValueRef, QuickError> {
-        let args: Vec<_> = args.iter().map(|arg| arg.val).collect();
+    pub fn call(
+        &self,
+        this: Option<JSValueRef>,
+        args: Vec<JSValueRef>,
+    ) -> Result<JSValueRef, QuickError> {
+        let args: Vec<_> = args.into_iter().map(|arg| arg.val()).collect();
 
         let value = unsafe {
-            let undefined = JS_MKVAL_real(sys::JS_TAG_UNDEFINED, 0);
+            let this = match this {
+                Some(v) => v.val(),
+                None => JS_MKVAL_real(sys::JS_TAG_UNDEFINED, 0),
+            };
 
             let result = sys::JS_Call(
                 self.value.ctx,
                 self.value.val,
-                undefined,
+                this,
                 args.len() as _,
                 args.as_ptr() as _,
             );
 
-            value::JS_FreeValue_real(self.value.ctx, undefined);
+            for arg in args {
+                value::JS_FreeValue_real(self.value.ctx, arg);
+            }
+            value::JS_FreeValue_real(self.value.ctx, this);
 
             result
         };
